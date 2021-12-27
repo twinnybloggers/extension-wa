98538e84e3fcb6acaaa96953075a2f04753bb451
kangriandotnet <72417953+kangriandotnet@users.noreply.github.com>
2021-12-27 18:50:25 +0700

    Update simstore-blogger-template.js

--- template/simstore-blogger-template.js
+++ template/simstore-blogger-template.js
@@ -6,7 +6,7 @@
 
     Mengubah / Menghapus file javascript (js) ini dapat mengakibatkan :
 
-    1. Kerusakan / error pada tempalte
+    1. Kerusakan / error pada template
     2. Menghilangkan fungsi update / perbaikan otomatis pada template
     3. Tidak akan mendapatkan support/bantuan dari kreator template
 
@@ -15,7 +15,7 @@
     Salam,
     KangRian.NET
 
-    Terakhir Diperbarui : 26/12/2021 @ 20:21
+    Terakhir Diperbarui : 27/12/2021 @ 18:50
 
 ================================================================================================ */
 
